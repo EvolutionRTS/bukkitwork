2f16045e32e3af538a06beac0ef0322df2cd50e2
ForbodingAngel <forbodingangel@gmail.com>
2014-06-08 21:22:41 -0700

    Initial commit

---
+++ gamedata/configs/paintjobs/smoth/default_outer_colonies.lua
@@ -0,0 +1,25 @@
+local colors	= {
+		paintR				= "grey_20",
+		sheenR				= "black_matte",
+		--texture projection map setting
+		projectionGlmode1		= '',
+		projectionTexture1 		= '',
+		projectionMatrix1	= '',
+		
+		paintG				= "taupe",
+		sheenG				= "black_matte",
+		--texture projection map setting
+		projectionGlmode2		= '',
+		projectionTexture2 		= '',
+		projectionMatrix2	= '',
+		
+		paintB				= "olivedrab",
+		sheenB				= "black_matte",
+		--does not have a texture projection map setting
+				
+		paintA				= "maroon",
+		sheenA				= "black_matte",
+		--does not have a texture projection map setting
+}
+
+return colors
